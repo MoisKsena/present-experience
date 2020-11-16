96b3817c0f04461af82a74ed168a82d743e913ca
Kseniia Moiseeva <mailtoksena@gmail.com>
2020-11-16 21:03:30 +0300

    footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,71 +1,76 @@
 <template>
   <div class="footer">
-    <div class="footer__promotion">
-      <div class="announcements">
-        <div class="footer__title">АНОНСЫ</div>
-        <div class="announcements__wrapper">
-          <div class="announcements__item">
-            <div class="announcements__item-image">
-              <img src="@/assets/image/footer/announce.png" alt="">
-            </div>
-            <div class="announcements__item-text">
-              Скатков Андрей.
-              Мария Орлова.
-              Одежда и несвобода.
+    <div class="footer__wrapper">
+      <div class="footer__promotion">
+        <div class="announcements">
+          <div class="footer__title">АНОНСЫ</div>
+          <div class="announcements__wrapper">
+            <div class="announcements__item">
+              <div class="announcements__item-image">
+                <img src="@/assets/image/footer/announce.png" alt="">
+              </div>
+              <div class="announcements__item-text">
+                Скатков Андрей.
+                Мария Орлова.
+                Одежда и несвобода.
+              </div>
             </div>
           </div>
+          
+          
+          
         </div>
-        
-        
-        
-      </div>
-      <div class="projects">
-        <div class="footer__title">ДРУГИЕ ПРОЕКТЫ</div>
-        <div class="projects__wrapper">
-          <div class="projects__item">
-          <div class="projects__item-image">
-            <img src="@/assets/image/footer/proj1.png" alt="">
-          </div>
-          <div class="projects__item-text">
-            Электронный архив
-            «Российская
-            повседневность»
-          </div>
-          </div>
-          <div class="projects__item">
+        <div class="projects">
+          <div class="footer__title">ДРУГИЕ ПРОЕКТЫ</div>
+          <div class="projects__wrapper">
+            <div class="projects__item">
             <div class="projects__item-image">
-              <img src="@/assets/image/footer/proj2.png" alt="">
+              <img src="@/assets/image/footer/proj1.png" alt="">
             </div>
             <div class="projects__item-text">
-              Свод русского фольклора.
-              Былины. Звуковой аналог
+              Электронный архив
+              «Российская
+              повседневность»
+            </div>
+            </div>
+            <div class="projects__item">
+              <div class="projects__item-image">
+                <img src="@/assets/image/footer/proj2.png" alt="">
+              </div>
+              <div class="projects__item-text">
+                Свод русского фольклора.
+                Былины. Звуковой аналог
+              </div>
+            </div>
+            <div class="projects__item">
+              <div class="projects__item-image">
+                <img src="@/assets/image/footer/proj3.png" alt="">
+              </div>
+              <div class="projects__item-text">
+                Русский фольклор
+                в современных записях
+              </div>
+            </div>
+            <div class="projects__item">
+              <div class="projects__item-image">
+                <img src="@/assets/image/footer/proj4.png" alt="">
+              </div>
+              <div class="projects__item-text">
+                Фольклор
+                и фольклористика СПбГУ
+              </div>
             </div>
           </div>
-          <div class="projects__item">
-            <div class="projects__item-image">
-              <img src="@/assets/image/footer/proj3.png" alt="">
-            </div>
-            <div class="projects__item-text">
-              Русский фольклор
-              в современных записях
-            </div>
-          </div>
-          <div class="projects__item">
-            <div class="projects__item-image">
-              <img src="@/assets/image/footer/proj4.png" alt="">
-            </div>
-            <div class="projects__item-text">
-              Фольклор
-              и фольклористика СПбГУ
-            </div>
-          </div>
+          
         </div>
-        
       </div>
     </div>
+    
     <div class="footer__copyright">
-      <p>© Прагмема 2011-2020</p>
-      <p>Студия «M» — 2020</p>
+      <div class="footer__wrapper m-copyright">
+        <p>© Прагмема 2011-2020</p>
+        <p>Студия «M» — 2020</p>
+      </div>
     </div>
   </div>
   
