fe5f553ba62da013c3ea4bd258b9ac2e4da7a7e5
Kseniia Moiseeva <mailtoksena@gmail.com>
2020-11-16 21:48:07 +0300

    *

--- src/router.js
+++ src/router.js
@@ -12,7 +12,7 @@ const routes = [
     path: '/',
     //name: 'Experience',
     //component: Experience,
-    redirect: '/experience'
+    redirect: 'experience'
   },
   {
     path: 'experience',
